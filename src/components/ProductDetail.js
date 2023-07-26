8a3be5e05456adfccce531391d252cec08b30702
CipHer170 <105492413+CipHer170@users.noreply.github.com>
2023-07-26 10:11:57 +0500

    scss styling

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,8 +1,8 @@
-import { Stack } from "@mui/material";
+import { Button, Stack } from "@mui/material";
 import React, { useContext, useState } from "react";
-import "./LearnMore.scss";
 import { ProductContext } from "../context/productContext";
 import { useParams } from "react-router-dom";
+import "./productDetail.scss";
 
 function ProductDetail({ id }) {
   const { product = [] } = useContext(ProductContext);
@@ -10,28 +10,57 @@ function ProductDetail({ id }) {
   const uniqueId = ProductId.id;
   const productDetail = product.find((item) => item.id === uniqueId);
 
-  const [getData, setGetData] = useState(false);
-  if (id === uniqueId) {
-    setGetData(!getData);
+  const { image, description, title, amount, price } = productDetail || {};
+
+  const images = [
+    {
+      id: 1,
+      img: "https://svgsilh.com/svg/324596.svg",
+    },
+    {
+      id: 2,
+      img: "https://www.svgrepo.com/show/117055/small-duck.svg",
+    },
+    {
+      id: 3,
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Linecons_small-cloud.svg/512px-Linecons_small-cloud.svg.png?20160307130929",
+    },
+    {
+      id: 4,
+      img: "https://svgsilh.com/svg/312035.svg",
+    },
+  ];
+
+  if (productDetail === undefined) {
+    return null;
   }
+  console.log(productDetail);
   return (
-    <Stack className="product">
-      <h4>{JSON.stringify(productDetail, null, 2)}</h4>
+    <Stack className="product" display={"flex"} flexDirection={"row"}>
+      <Stack className="product__images">
+        <Stack className="product__images_list">
+          {images.map((img) => {
+            return <img src={img.img} alt="images fruit" />;
+          })}
+        </Stack>
+        <Stack className="product__images_image">
+          <img alt="bkg" src={image} />
+        </Stack>{" "}
+      </Stack>
+      <Stack className="product__action ">
+        <Stack className="product__action_mainInfo">{title}</Stack>
+        <Stack className="product__action_amount ">
+          Amount:
+          {amount}
+        </Stack>
+        <Stack className="product__action_price">Cost: {price}</Stack>
+        <Stack className="product__action_addCart">
+          <Button>Add</Button>
+        </Stack>
+        {/* <Stack className="product__action_description">{description}</Stack>{" "} */}
+      </Stack>
     </Stack>
   );
 }
 
 export default ProductDetail;
-//  {/* <Stack className="product__images">
-//       <Stack className="product__image_list">sdfsfdfsd</Stack>
-//       <Stack className="product__image_image">
-//         <img alt="bkg" />
-//       </Stack>
-//     </Stack>
-//     <Stack className="product__action">
-//       <Stack className="product__action_mainInfo"></Stack>
-//       <Stack className="product__action_type"></Stack>
-//       <Stack className="product__action_amount"></Stack>
-//       <Stack className="product__action_price"></Stack>
-//       <Stack className="product__action_addCart"></Stack>
-//     </Stack> */}
