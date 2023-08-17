b8f170f69df3a1a46c7fe21ad5075df5150a4700
CipHer170 <105492413+CipHer170@users.noreply.github.com>
2023-08-17 16:21:13 +0500

    fixed mobile

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <div className="logoBar"></div>
+      <div className="searchBar"></div>
+      <div className="user"></div>
+    </div>
+  );
+}
+
+export default Navbar;
