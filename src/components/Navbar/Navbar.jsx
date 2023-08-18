523f680a3d7141014985e9ed595558f29b407298
CipHer170 <105492413+CipHer170@users.noreply.github.com>
2023-08-18 12:50:01 +0500

    fixed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, {  useState } from "react";
+import React, { useState } from "react";
 import "../Navbar/navbar.scss";
 import logoImage from "../../assets/logo_img.svg";
 import { Link } from "react-router-dom";
