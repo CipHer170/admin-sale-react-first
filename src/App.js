98b2fe211b5f711adb24338e7068a0ed38734a4f
CipHer170 <105492413+CipHer170@users.noreply.github.com>
2023-08-18 15:43:41 +0500

    fixed styling

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.scss";
 import AppContainer from "./AppContainer";
 import Provider from "./context/productContext";
 import Navbar from "./components/Navbar/Navbar";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
 function App() {
   return (
     <Provider>
       <Navbar />
       <AppContainer />
+      <ScrollToTop />
     </Provider>
   );
 }
